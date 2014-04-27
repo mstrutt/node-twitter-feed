4935c3880ebcb176b51abc9784b84ef2e1d49a7b
Michael Strutt <michael@mstrutt.co.uk>
2014-04-27 17:27:49 +0100

    Refactoring and better error handling

--- tweets.js
+++ tweets.js
@@ -13,19 +13,61 @@ function tweetText (tweet) {
 	return tweet.text;
 }
 
-function getAge (file) {
-	var stat = fs.statSync(config.filename),
-		now = new Date().getTime(),
-		lastMod = new Date(stat.mtime).getTime();
-		age = Math.round((now - lastMod) / 1000);
-	console.log(age + 's since last update');
-	return age;
-}
-
 function fourOhFour (request, response) {
 	response.statusCode = 404;
 	response.end('404');
 	console.log(request.url+' went 404 :(');
+}
+
+function sendCachedTweets (request, response) {
+	console.log('Serving tweets from cache');
+	fs.readFile(config.filename, function(err, tweets){
+		if (err) {
+			console.log(err);
+			response.statusCode = 500;
+			response.end('Error');
+		} else {
+			response.end(tweets);
+		}
+	});	
+}
+
+function getNewTweets (request, response) {
+	twitter.get('statuses/user_timeline', {
+		screen_name: config.handle,
+		trim_user: true,
+		exclude_replies: true
+	}, function(err, tweets) {
+		if (err) {
+			console.log(err);
+			sendCachedTweets(request, response);
+		} else {
+			tweets = JSON.stringify(tweets.slice(0, config.count).map(tweetText));
+			response.end(tweets);
+			console.log('Serving up-to-date tweets');
+			fs.writeFile(config.filename, tweets, function(err) {
+				if (!err) console.log('Tweets saved to cache');
+			});
+		}
+	});
+}
+
+function respondWithTweets (request, response) {
+	fs.stat(config.filename, function(err, stat) {
+		if (err) console.log(err);
+		
+		var	now = new Date().getTime(),
+			lastMod = stat ? new Date(stat.mtime).getTime() : 0,
+			age = Math.round((now - lastMod) / 1000);
+		
+		console.log(age + 's since last update');
+	
+		if (age > 300) {
+			getNewTweets(request, response);
+		} else {
+			sendCachedTweets(request, response)
+		}
+	});
 }
 
 http.createServer(function (request, response) {
@@ -33,31 +75,7 @@ http.createServer(function (request, response) {
 	if (request.url.indexOf(config.filename) === -1) {
 		fourOhFour(request, response);
 	} else {
-		if (getAge(config.filename) > 300) {
-			twitter.get('statuses/user_timeline', {
-				screen_name: config.handle,
-				trim_user: true,
-				exclude_replies: true
-			}, function(err, tweets) {
-				if (err)
-					console.log(err);
-				else {
-					tweets = JSON.stringify(tweets.slice(0, config.count).map(tweetText));
-					fs.writeFile(config.filename, tweets);
-					console.log('Tweets saved to cache');
-					response.end(tweets);
-					console.log('Serving up-to-date tweets');
-				}
-			});
-		} else {
-			console.log('Serving tweets from cache');
-			fs.readFile(config.filename, function(err, tweets){
-				if (err)
-					console.log(err);
-				else
-					response.end(tweets);
-			});		
-		}
+		respondWithTweets(request, response);
 	}
 }).listen(config.port, config.ip);
 
