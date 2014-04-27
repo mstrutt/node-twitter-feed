f471eb530ec7be51d8f803e51003065d2bfa91f5
Michael Strutt <michael@mstrutt.co.uk>
2014-04-27 13:47:43 +0100

    adding in primative caching and using map for extracting tweets

--- tweets.js
+++ tweets.js
@@ -1,26 +1,37 @@
 var config = require('./config.json').twitter,
 	Twitter = require('mtwitter'),
-	fs = require('fs');
+	fs = require('fs'),
 
-var twitter = new Twitter({
-	consumer_key: config.key,
-	consumer_secret: config.secret,
-	application_only: true
-});
+	twitter = new Twitter({
+		consumer_key: config.key,
+		consumer_secret: config.secret,
+		application_only: true
+	}),
 
-twitter.get('statuses/user_timeline', {
-	screen_name: config.handle,
-	trim_user: true,
-	exclude_replies: true
-}, function(err, item) {
-	if (err)
-		console.log(err);
-	else {
-		var tweets = [];
-		item.forEach(function(tweet){
-			tweets.push(tweet.text);
-		});
-		fs.writeFileSync('tweets.json', JSON.stringify(tweets));
-	}
+	tweetText = function(tweet){
+		return tweet.text;
+	},
+
+	stat = fs.statSync('tweets.json'),
+	now = new Date().getTime(),
+	lastMod = new Date(stat.ctime).getTime(),
+	age = now - lastMod;
+
+if (age >  + 300000) {
+	twitter.get('statuses/user_timeline', {
+		screen_name: config.handle,
+		trim_user: true,
+		exclude_replies: true
+	}, function(err, feed) {
+		if (err)
+			console.log(err);
+		else {
+			fs.writeFileSync('tweets.json', JSON.stringify(feed.map(tweetText)));
+			console.log('Tweets save to tweets.json');
+		}
+		process.exit();
+	});
+} else {
+	console.log('Using cached version ('+Math.floor(age / 1000)+'s)');
 	process.exit();
-});
+}
